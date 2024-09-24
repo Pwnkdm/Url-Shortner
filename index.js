f43745d7cfc8f9dfb420c95544a318ca3e341141
Pwnkdm <pawanrajkadam@gmail.com>
2024-09-24 23:16:25 +0530

    change made

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-const express = require('express');
-const urlRoute = require('./routes/url.routes');
+const express = require("express");
+const urlRoute = require("./routes/url.routes");
 const { connectToMongoDB } = require("./connect");
 const URL = require("./models/url");
-require('dotenv').config()
-const cors = require('cors');
+require("dotenv").config();
+const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: "*" }));
 
-connectToMongoDB(`mongodb+srv://${process.env.USERNAME}:${process.env.PASS}@cluster0.inutweg.mongodb.net/url-shortner?retryWrites=true&w=majority`).then(()=>{
-console.log("MogoDb Connected!");
-})
+connectToMongoDB(
+  `mongodb+srv://${process.env.USERNAME}:${process.env.PASS}@cluster0.inutweg.mongodb.net/url-shortner?retryWrites=true&w=majority`
+).then(() => {
+  console.log("MogoDb Connected!");
+});
 
 app.use(express.json());
 
-app.use('/url',urlRoute);
+app.use("/url", urlRoute);
 
-app.listen(process.env.PORT||8080,()=>{
-    console.log(`Server listening on ${process.env.PORT || 8080}`);
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Server listening on ${process.env.PORT || 8080}`);
 });
